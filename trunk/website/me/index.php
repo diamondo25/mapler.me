1cc7a93061cce4ad0ca560706a558d86b219afce
thebluecorsair <tyler@tylerliberman.com>
2012-12-07 06:35:18 +0000

    Major update.
    Created the /me/ directory with blank pages that will be worked on later.
    
    my-characters.php will display all current characters on a player's account.
    
    character-loader.php will be what displays a individual character by itself (own page) and can be accessible through /me/charnamehere
    
    my-friends.php (will likely be renamed my-buddies) will contain all of the player's current buddies, which will use an API by Nexon built into their game to allow communication with players in-game.
    
    Created /me/panel.
    -Settings allows for changing of general settings, account information, privacy options, and security.
    
    -Memberships will be features such as developer, as well as all current memberships the player is including if one is a registered Nexon staff / us (admin). This will have to be added to the database with each membership having a certain number.
    
    Couple of other changes.
    
    git-svn-id: https://subversion.assembla.com/svn/mapler-me@14 7cbc8f23-9b70-4854-b091-be65f742de0c

---
+++ trunk/website/me/index.php
@@ -0,0 +1,26 @@
+<?php include_once('inc/header.php'); ?>
+      <div class="jumbotron">
+        
+        <div id="myCarousel" class="carousel slide">
+		  <!-- Carousel items -->
+		  <div class="carousel-inner">
+			<div class="active item"><h1>Are you ready to become a better mapler?</h1>
+				<p class="lead"><img src="https://dl.dropbox.com/u/22875564/mapler.me.resources/front1.gif" class="pull-right"/>Join hundreds of other maplers and keep track of your progress in-game, as well as socialize with your buddies in-game!</p>
+				</div>
+			<div class="item"><h1>Keep track of your own Maple 'Story'!</h1>
+				<p class="lead"><img src="https://dl.dropbox.com/u/22875564/mapler.me.resources/front3.gif" class="pull-left"/>Mapler.me records everything from your experience, stats, and even past looks!<br/>Never forget a past look again!</p>
+			 </div>
+			<div class="item"><h1>Safe, fast, and secure.<br/>It just <a href="#">works.</a></h1>
+				<p class="lead"><img src="https://dl.dropbox.com/u/22875564/mapler.me.resources/front2.gif" class="pull-right"/>Mapler.me keeps all player information confidential and secure!<br/>
+					We are partnered with Nexon America, and is built with perfection.
+				</p></div>
+		  </div>
+		  <!-- Carousel nav -->
+		  <a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
+		  <a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
+		</div>
+
+	<a class="btn btn-small" href="/register" role="button" style="">What are you waiting for? Sign up!</a>
+      </div>
+
+<?php include_once('inc/footer.php'); ?>
