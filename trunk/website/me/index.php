e65c2504dd0d41703b2b06dcc4f7425517e31e40
thebluecorsair <tyler@tylerliberman.com>
2012-12-12 19:54:47 +0000

    - Added characters temporarily to the profile. It'd be better to be able to be able to select which characters you want displayed through the panel. - Removed .htaccess ip block, added login through .htaccess. - Will state passwords on Whatsapp. / It wasn't working for me on my local testing, so seeing if they work. - Added .htpasswd for login.
    git-svn-id: https://subversion.assembla.com/svn/mapler-me@32 7cbc8f23-9b70-4854-b091-be65f742de0c

--- trunk/website/me/index.php
+++ trunk/website/me/index.php
@@ -10,6 +10,102 @@ include_once('../inc/header.php');
 	<div class="span10">
 	<p class="lead"><?php echo $__url_userdata['full_name']; ?> <span class="muted">(<?php echo $__url_userdata['nickname']; ?>)</span></p>
 	<?php echo $__url_userdata['bio']; ?>
+	
+	<hr/>
+	
+	<!-- Character Display -->
+	<div class="span10">
+
+<script>
+function ChangeImage(id, name) {
+	document.getElementById('image_' + id).src = "//<?php echo $domain; ?>/actions/character_image.php?name=" + name;
+	document.getElementById('stats_' + id).src = "//<?php echo $domain; ?>/actions/character_stats.php?name=" + name;
+}
+</script>
+<?php
+
+
+
+
+
+$q = $__database->query("
+SELECT 
+	chr.id, 
+	chr.name, 
+	w.world_name 
+FROM 
+	characters chr 
+LEFT JOIN 
+	users usr 
+	ON 
+		usr.ID = chr.userid 
+LEFT JOIN 
+	accounts acc 
+	ON 
+		acc.id = usr.account_id 
+LEFT JOIN 
+	world_data w 
+	ON 
+		w.world_id = chr.world_id 
+
+WHERE 
+	acc.username = '".$__database->real_escape_string($__url_userdata['username'])."' 
+ORDER BY 
+	chr.world_id ASC,
+	chr.level DESC
+");
+
+if ($q->num_rows == 0) {
+?>
+<p class="lead alert-error alert"><?php echo $__url_userdata['username']; ?> hasn't added any characters yet!</p>
+
+<?php
+}
+
+	
+$last_world = NULL;
+while ($row = $q->fetch_row()) {
+	if ($last_world != $row[2]) {
+		if ($last_world != NULL) {
+?>
+</table>
+</fieldset>
+<?php
+		}
+?>
+<fieldset>
+<legend><button class="btn" data-toggle="collapse" data-target="#<?php echo $row[2]; ?>" href="#<?php echo $row[2]; ?>"><?php echo $row[2]; ?></button></legend>
+<div id="<?php echo $row[2]; ?>" class="collapse accordion-body">
+<table width="100%">
+
+<?php
+		$last_world = $row[2];
+	}
+?>
+	<tr>
+		<td><?php echo $row[1]; ?></td>
+		<td><img src="//<?php echo $domain; ?>/inc/img/char_bg.png" alt="Image of <?php echo $row[1]; ?>" id="image_<?php echo $row[0]; ?>" width="271px" height="162px" /></td>
+		<td><img src="//<?php echo $domain; ?>/inc/img/stat_window.png" alt="Statistics of <?php echo $row[1]; ?>" id="stats_<?php echo $row[0]; ?>" width="192px" height="345px" /></td>
+	</tr>
+<?php
+}
+
+$q->data_seek(0);
+?>
+</table>
+</div>
+</fieldset>
+<script>
+<?php
+	while ($row = $q->fetch_row()) {
+?>
+ChangeImage(<?php echo $row[0]; ?>, '<?php echo $row[1]; ?>');
+<?php
+	
+	}
+?>
+</script>
+	
 	</div>
 	</div>
 
