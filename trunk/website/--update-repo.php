443027b4f31a660a664c22abbf7eab078ad67356
diamondo25 <blablaechthema@hotmail.com>
2012-12-12 17:42:52 +0000

    Updated update-repo. Now showing current version
    git-svn-id: https://subversion.assembla.com/svn/mapler-me@29 7cbc8f23-9b70-4854-b091-be65f742de0c

--- trunk/website/--update-repo.php
+++ trunk/website/--update-repo.php
@@ -1,3 +1,36 @@
+<?php
+function RunCMD($cmd) {
+	$descriptorspec = array(
+	   0 => array("pipe", "r"),  // stdin is a pipe that the child will read from
+	   1 => array("pipe", "w"),  // stdout is a pipe that the child will write to
+	   2 => array("file", "/tmp/error-output.txt", "a") // stderr is a file to write to
+	);
+	
+	$cwd = '/mal';
+	
+	$process = proc_open($cmd, $descriptorspec, $pipes, $cwd);
+	
+	if (is_resource($process)) {
+		$data = stream_get_contents($pipes[1]);
+		fclose($pipes[1]);
+	
+		$return_value = proc_close($process);
+	}
+	else {
+		$data = "ERROR";
+	}
+	
+	return $data;
+}
+?>
+
+<pre>
+<?php
+$rows = explode("\n", RunCMD('svn info /var/www/maplestats_svn/ 2>&1'));
+echo $rows[7]." - ".$rows[8]." - ".$rows[9];
+?>
+</pre>
+
 <form action="" method="post">
 <input type="password" name="ihewfihewfewf" />
 <input type="submit" />
@@ -10,7 +43,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && $_POST['ihewfihewfewf'] == 'HURR1312
 	$username = "maplerme-website";
 	$password = "#FMO@JF)JNRWGO$@Ngf9hwref923@R#@";
 
-	system('svn up --non-interactive --username '.escapeshellarg($username).' --password '.escapeshellarg($password).' /var/www/maplestats_svn/ 2>&1');
+	echo RunCMD('svn up --non-interactive --username '.escapeshellarg($username).' --password '.escapeshellarg($password).' /var/www/maplestats_svn/ 2>&1');
+
 }
 ?>
 </pre>
