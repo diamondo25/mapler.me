8f30f1b0b23788c991914d1543d4d736e9f70db1
thebluecorsair <tyler@tylerliberman.com>
2013-01-15 20:15:49 +0000

    - Added our email template for sending emails (currently written as the registration email). Please added it into the form's confirmation once the register form is passed successfully.
    - Added additional ranks:
    BANNED = -100 (would not show / disable most or all links in menu + show an "unban request" link.
    AWAITING_ACTIVATION = 0 (certain features such as adding characters + their profile disabled)
    NORMAL = 100 (same, all normal features accessible)
    DEVELOPER = 200 (gives additional links / access to developer tools, API, etc)
    NEXON = 300 (a special rank given to any Nexon employees to be able to monitor player information more directly, likely required as apart of a partnership.
    MODERATOR = 900 (any staff that help monitor / handle issues on the site with players. NOT TECHINCAL ISSUES)
    ADMIN = 1000 (Full access to all features on the site, able to pass revisions to the repo, and can resolve technical issues through a panel that will be coded.
    
    I wanted to work on the forms within the Settings page although I couldn't match / work with your Form function correctly, so I'll let you handle that when the time comes, especially since you're more familiar with how your password salting program works.
    
    Another commit in a bit.
    
    git-svn-id: https://subversion.assembla.com/svn/mapler-me@52 7cbc8f23-9b70-4854-b091-be65f742de0c

--- trunk/website/inc/ranks.php
+++ trunk/website/inc/ranks.php
@@ -1,4 +1,12 @@
 <?php
 
-define("RANK_NORMAL", 0);
+// Members
+define("RANK_BANNED", -100);
+define("RANK_AWAITING_ACTIVATION", 0);
+define("RANK_NORMAL", 100);
+define("RANK_DEVELOPER", 200);
+define("RANK_NEXON", 300);
+
+// Staff
+define("RANK_MODERATOR", 900);
 define("RANK_ADMIN", 1000);
