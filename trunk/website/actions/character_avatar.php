e72051dec79cd990c9fbc68a29814736f751c3be
diamondo25 <blablaechthema@hotmail.com>
2013-01-16 23:07:33 +0000

    randomfaic
    git-svn-id: https://subversion.assembla.com/svn/mapler-me@61 7cbc8f23-9b70-4854-b091-be65f742de0c

--- trunk/website/actions/character_avatar.php
+++ trunk/website/actions/character_avatar.php
@@ -602,8 +602,10 @@ else {
 		add_image($mask_location, $mainx + $maskx, $mainy + $masky);
 	}
 	
+	$faces = array("angry", "bewildered", "blaze", "bowing", "cheers", "chu", "cry", "dam", "despair", "glitter", "hit", "hot", "love");
+	
 	// Create face
-	$face_location = "$characterwz/Face/000".$face.".img/default.face.png";
+	$face_location = "$characterwz/Face/000".$face.".img/".$faces[rand(0, count($faces)].".0.face.png";
 	add_image($face_location, $mainx + $facex, $mainy + $facey);
 	
 	// Create mask
