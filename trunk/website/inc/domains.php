658b4777fd2cdd06a714a85c89e78d35c9210210
diamondo25 <blablaechthema@hotmail.com>
2013-01-21 20:49:57 +0000

    Wups missed a file
    git-svn-id: https://subversion.assembla.com/svn/mapler-me@72 7cbc8f23-9b70-4854-b091-be65f742de0c

---
+++ trunk/website/inc/domains.php
@@ -0,0 +1,33 @@
+<?php
+$subdomain = "";
+$domain = "";
+if (strpos($_SERVER['SERVER_NAME'], "direct.mapler.me") !== false) {
+	// SOMETHING.direct.mapler.me
+	$subdomain = substr($_SERVER['SERVER_NAME'], 0, strrpos($_SERVER['SERVER_NAME'], ".direct.mapler.me"));
+	$domain = "direct.mapler.me";
+}
+elseif (strpos($_SERVER['SERVER_NAME'], "mapler.me") !== false) {
+	// SOMETHING.mapler.me
+	$subdomain = substr($_SERVER['SERVER_NAME'], 0, strrpos($_SERVER['SERVER_NAME'], ".mapler.me"));
+	$domain = "mapler.me";
+}
+elseif (strpos($_SERVER['SERVER_NAME'], "mplrtest.craftnet.nl") !== false) {
+	// SOMETHING.mplrtest.craftnet.nl << Test Case Server
+	$subdomain = substr($_SERVER['SERVER_NAME'], 0, strrpos($_SERVER['SERVER_NAME'], ".mplrtest.craftnet.nl"));
+	$domain = "mplrtest.craftnet.nl";
+}
+elseif (strpos($_SERVER['SERVER_NAME'], "mplr.e.craftnet.nl") !== false) {
+	// SOMETHING.mplr.e.craftnet.nl << Test Case Erwin
+	$subdomain = substr($_SERVER['SERVER_NAME'], 0, strrpos($_SERVER['SERVER_NAME'], ".mplr.e.craftnet.nl"));
+	$domain = "mplr.e.craftnet.nl";
+}
+
+elseif (strpos($_SERVER['SERVER_NAME'], "website") !== false) {
+	// SOMETHING.website << Local Testing Tyler
+	$subdomain = substr($_SERVER['SERVER_NAME'], 0, strrpos($_SERVER['SERVER_NAME'], ".website:404"));
+	$domain = "website:404";
+}
+
+$subdomain = trim($subdomain);
+
+?>
