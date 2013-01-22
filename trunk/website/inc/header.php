1c2c00687148c22c17151aa55a51d8d101753aac
thebluecorsair <tyler@tylerliberman.com>
2013-01-22 18:04:51 +0000

    - Added new rewrite. - Major theme revamp + design. Expect broken designs / flaws - Added base Bootstrap to style.css + modified.
    Please look for issues.
    
    git-svn-id: https://subversion.assembla.com/svn/mapler-me@74 7cbc8f23-9b70-4854-b091-be65f742de0c

--- trunk/website/inc/header.php
+++ trunk/website/inc/header.php
@@ -32,54 +32,29 @@ if ($subdomain != "" && $subdomain != "www" && $subdomain != "direct" && $subdom
 }
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <title>Mapler.me &middot; Official MapleStory Social Network</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <!-- Le styles -->
-    <link href="//<?php echo $domain; ?>/inc/css/bootstrap.min.css" rel="stylesheet">
-    <link href="//<?php echo $domain; ?>/inc/css/style.css" rel="stylesheet">
-    <link href="//thebluecorsair.com/includes/font-awesome/css/font-awesome.css" rel="stylesheet">
-    <style type="text/css">
-      body {
-        padding-top: 20px;
-      }
-    </style>
-
-    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
-    <!--[if lt IE 9]>
-      <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
-    <![endif]-->
-
-  </head>
+<head>
+	<title>Mapler.me &middot; MapleStory Social Network</title>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="keywords" content="maplestory, maple, story, mmorpg, maple story, maplerme, mapler, me, ">
+		<meta name="description" content="Mapler.me is a MapleStory social network, with innovative features!">
+		
+		<link href='http://fonts.googleapis.com/css?family=Muli:300,400,300italic,400italic' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="//<?php echo $domain; ?>/inc/css/style.css" type="text/css" />
+</head>
 
 <body>
 
-	<div class="container">
-
-		<div class="navbar">
-			<div class="navbar-inner">
-				<div class="container">
-
-					<!-- .btn-navbar is used as the toggle for collapsed navbar content -->
-					<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-					</a>
-
-					<img src="//<?php echo $domain; ?>/inc/img/logo.gif" class="pull-left" style="position:relative;right:10px;top:3px;"/>
-					<!-- Be sure to leave the brand out there if you want it shown -->
-					<a class="brand" href="//<?php echo $domain; ?>/"><strong>Mapler</strong>.me</a>
-
-					<!-- Everything you want hidden at 940px or less, place within here -->
-					<div class="nav-collapse collapse">
-					<ul class="nav">
+	<div class="navbar navbar-fixed-top">
+		<div class="navbar-inner">
+			<div class="container">
+				<a class="brand" href="/" style="margin-top: 8px;opacity: 1;color: #fff3e4;text-decoration: none;text-shadow: 0 -1px 0 rgba(0,0,0,0.25);"><img src="http://website:404/inc/img/logo.gif" style="float:left;position:relative;bottom:5px;right:5px;"/>Mapler.me</a>
+			<div class="nav-collapse">
+				<ul class="nav hidden-phone">
+					 <li class="dropdown">
+					 	<a data-toggle="dropdown" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-delay="100" data-close-others="true" href="#"> Pages <b class="caret"></b></a>
+					 		<ul class="dropdown-menu">
 <?php
 if (isset($__url_userdata)):
 ?>
@@ -92,17 +67,22 @@ else:
 <?php
 endif;
 ?>
-					</ul>
-					<ul class="nav pull-right">
-						<li id="fat-menu" class="dropdown">
+					 		</ul>
+					 </li>
+				</ul>
+				
+			<!-- Login / Main Menu -->	
+				<ul class="nav hidden-phone pull-right">
+					 <li class="dropdown">
 <?php
 if ($_loggedin):
 ?>
-							<a href="#" id="drop3" role="button" class="dropdown-toggle" data-toggle="dropdown">Welcome back, <?php echo $_logindata['full_name']; ?></a>
-							<ul class="dropdown-menu" role="menu" aria-labelledby="drop3">
-								<li id="fat-menu"><a href="//<?php echo $_logindata['username']; ?>.<?php echo $domain; ?>/">Profile</a></li>
-								<li id="fat-menu"><a href="//<?php echo $_logindata['username']; ?>.<?php echo $domain; ?>/my-characters">My Characters</a></li>
-								<li id="fat-menu"><a href="//<?php echo $_logindata['username']; ?>.<?php echo $domain; ?>/panel/">Settings</a></li>
+					 	<a data-toggle="dropdown" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-delay="100" data-close-others="true" href="#"> Welcome back! <?php echo $_logindata['full_name']; ?> <b class="caret"></b></a>
+					 		<ul class="dropdown-menu">
+						<li><a href="//<?php echo $_logindata['username']; ?>.<?php echo $domain; ?>/">Profile</a></li>
+						<li><a href="//<?php echo $_logindata['username']; ?>.<?php echo $domain; ?>/my-characters">My Characters</a></li>
+						<li><a href="//<?php echo $_logindata['username']; ?>.<?php echo $domain; ?>/panel/">Settings</a></li>
+						
 <?php
 if ($_logindata['account_rank'] == RANK_ADMIN):
 ?>
@@ -111,15 +91,16 @@ if ($_logindata['account_rank'] == RANK_ADMIN):
 <?php
 endif;
 ?>
-								<li class="divider"></li>
-								<li id="fat-menu"><a href="//<?php echo $domain; ?>/logoff">Log off</a></li>
+
+	<li class="divider"></li>
+								<li><a href="//<?php echo $domain; ?>/logoff">Log off</a></li>
 							</ul>
 <?php
 else:
 ?>
-							<a href="#" id="drop3" role="button" class="dropdown-toggle" data-toggle="dropdown"><img src="//<?php echo $domain; ?>/inc/img/icons/user_go.png"/><b class="caret"></b></a>
-							<ul class="dropdown-menu" role="menu" aria-labelledby="drop3">
-								<form class="form-horizontal login" style="margin:10px;" action="//<?php echo $domain; ?>/login/" method="post">
+						<a data-toggle="dropdown" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-delay="100" data-close-others="true" href="#">Login <b class="caret"></b></a>
+						<ul class="dropdown-menu">
+						<form class="form-horizontal login" style="margin:10px;" action="//<?php echo $domain; ?>/login/" method="post">
 								<div class="control-group">
 									<div class="controls">
 										<input type="text" id="inputUsername" name="username" placeholder="Username" />
@@ -136,25 +117,87 @@ else:
 										<button type="button" onclick="document.location = 'http://<?php echo $domain; ?>/register/'" class="btn pull-right" style="display:none;">Register?</button>
 									</div>
 								</div>
-								</form>
-							</ul>
+						</ul>
 <?php
 endif;
 ?>
-						</li>
-					</ul>
-				</div>
+					 		</ul>
+					 </li>
+				</ul>
+<?php
+if (!$_loggedin):
+?>	
+			<ul class="nav pull-right hidden-phone">
+				<li id="nav-signup-btn"><a href="/signup/">Request Invite</a></li>
+				</ul>
+<?php
+endif;
+?>
 
+		
+		<ul class="nav mobile pull-right">
+			<li class="menu dropdown">
+				<a data-toggle="dropdown" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-delay="100" data-close-others="true" href="#"><span class="sprite more menu"></span></a>
+				
+							<ul class="dropdown-menu">
+<?php
+if ($_loggedin):
+?>
+						<li><a href="//<?php echo $_logindata['username']; ?>.<?php echo $domain; ?>/">Profile</a></li>
+						<li><a href="//<?php echo $_logindata['username']; ?>.<?php echo $domain; ?>/my-characters">My Characters</a></li>
+						<li><a href="//<?php echo $_logindata['username']; ?>.<?php echo $domain; ?>/panel/">Settings</a></li>
+						
+<?php
+if ($_logindata['account_rank'] == RANK_ADMIN):
+?>
+								<li class="divider"></li>
+								<li id="fat-menu"><a href="//<?php echo $domain; ?>/actions/repo/">Update Website</a></li>
+<?php
+endif;
+?>
+
+	<li class="divider"></li>
+								<li><a href="//<?php echo $domain; ?>/logoff">Log off</a></li>
+							</ul>
+<?php
+else:
+?>
+						<form class="form-horizontal login" style="margin:10px;" action="//<?php echo $domain; ?>/login/" method="post">
+								<div class="control-group">
+									<div class="controls">
+										<input type="text" id="inputUsername" name="username" placeholder="Username" />
+									</div>
+								</div>
+								<div class="control-group">
+									<div class="controls">
+										<input type="password" id="inputPassword" name="password" placeholder="Password" />
+									</div>
+								</div>
+								<div class="control-group">
+									<div class="controls">
+										<button type="submit" class="btn btn-success" style="margin-right:2px;width:220px;">Sign in</button>
+										<button type="button" onclick="document.location = 'http://<?php echo $domain; ?>/register/'" class="btn pull-right" style="display:none;">Register?</button>
+									</div>
+								</div>
+<?php
+endif;
+?>
+					 			
+					 		</ul>
+		</li>
+	</ul>
+			</div>
 			</div>
 		</div>
 	</div>
-	
+
+<div class="container">
 	<?php
 if (!$_loggedin):
 ?>
 
 	<p class="lead alert alert-error">&nbsp;Mapler.me is currently in private development. <b>Check back later!</b></p>
 	
-	<?php
+<?php
 endif;
 ?>	
