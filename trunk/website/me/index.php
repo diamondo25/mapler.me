07eb4c748b6a92f01c2b0532118362cfc95e1646
diamondo25 <blablaechthema@hotmail.com>
2013-01-22 23:33:11 +0000

    So close...
    git-svn-id: https://subversion.assembla.com/svn/mapler-me@86 7cbc8f23-9b70-4854-b091-be65f742de0c

--- trunk/website/me/index.php
+++ trunk/website/me/index.php
@@ -61,8 +61,8 @@ if (count($cache) > 0) {
 
 <script>
 function ChangeImage(id, name) {
-	document.getElementById('image_' + id).src = "//<?php echo $domain; ?>/actions/card/" + name;
-	document.getElementById('stats_' + id).src = "//<?php echo $domain; ?>/actions/infopic/" + name;
+	document.getElementById('image_' + id).src = "//<?php echo $domain; ?>/card/" + name;
+	document.getElementById('stats_' + id).src = "//<?php echo $domain; ?>/infopic/" + name;
 }
 </script>
 <?php
