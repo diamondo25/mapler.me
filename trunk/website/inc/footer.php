0399c6db01c062655bb3175c4bc1023fca926098
thebluecorsair <tyler@tylerliberman.com>
2013-02-25 18:24:01 +0000

    - Moved stats to footer (more reliable)
    git-svn-id: https://subversion.assembla.com/svn/mapler-me@246 7cbc8f23-9b70-4854-b091-be65f742de0c

--- trunk/website/inc/footer.php
+++ trunk/website/inc/footer.php
@@ -16,6 +16,20 @@
 	<script src="//cdnjs.cloudflare.com/ajax/libs/modernizr/2.6.2/modernizr.min.js" type="text/javascript"></script>
 	<script src="//isotope.metafizzy.co/jquery.isotope.min.js" type="text/javascript"></script>
 	
+	<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-36861298-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+	
 	<script>
     $(document).ready(function() {
         var container = $('#character-wall');
