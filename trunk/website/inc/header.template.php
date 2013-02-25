048cf54a8c9e190e50bc2935c8e678a34812f95d
thebluecorsair <tyler@tylerliberman.com>
2013-02-25 09:01:51 +0000

    - Added stats.
    git-svn-id: https://subversion.assembla.com/svn/mapler-me@244 7cbc8f23-9b70-4854-b091-be65f742de0c

--- trunk/website/inc/header.template.php
+++ trunk/website/inc/header.template.php
@@ -11,6 +11,21 @@
 	
 	<link href='http://fonts.googleapis.com/css?family=Muli:300,400,300italic,400italic' rel='stylesheet' type='text/css' />
 	<link rel="stylesheet" href="//<?php echo $domain; ?>/inc/css/style.css" type="text/css" />
+
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-36861298-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
 </head>
 
 <body>
