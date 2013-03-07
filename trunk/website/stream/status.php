8013fdd66609b1a8eae3e46ab85b575a5b86b4e3
thebluecorsair <tyler@tylerliberman.com>
2013-03-07 16:29:29 +0000

    - Reverting.
    git-svn-id: https://subversion.assembla.com/svn/mapler-me@305 7cbc8f23-9b70-4854-b091-be65f742de0c

--- trunk/website/stream/status.php
+++ trunk/website/stream/status.php
@@ -126,6 +126,15 @@ foreach ($cache as $row) {
 			
 			<div class="status">
 			<div id="disqus_thread"></div>
+			<script type="text/javascript">
+				var disqus_shortname = 'maplerme';
+				var disqus_url = '<?php echo $_SERVER['REQUEST_URI']; ?>';
+				(function() {
+				var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+				dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+				(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+				})();
+			</script>
 			</div>
         
 <?php       
