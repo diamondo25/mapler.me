f9dc3910fb7a98ac3c2100cc34c0380fe7a0f76e
thebluecorsair <tyler@tylerliberman.com>
2013-03-07 16:28:38 +0000

    - Moved scripts to footer. - Stuffs.
    git-svn-id: https://subversion.assembla.com/svn/mapler-me@304 7cbc8f23-9b70-4854-b091-be65f742de0c

--- trunk/website/inc/footer.php
+++ trunk/website/inc/footer.php
@@ -51,6 +51,16 @@
         	(document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
         	}());
         </script>
+        
+		<script type="text/javascript">
+			var disqus_shortname = 'maplerme';
+			var disqus_url = '<?php echo $_SERVER['REQUEST_URI']; ?>';
+			(function() {
+			var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+			dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+			(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+			})();
+		</script>        
 
 	<script type="text/javascript">
     $(document).ready(function() {
--- trunk/website/stream/status.php
+++ trunk/website/stream/status.php
@@ -126,14 +126,6 @@ foreach ($cache as $row) {
 			
 			<div class="status">
 			<div id="disqus_thread"></div>
-			<script type="text/javascript">
-				var disqus_shortname = 'maplerme';
-				(function() {
-				var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-				dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-				(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-				})();
-			</script>
 			</div>
         
 <?php       
