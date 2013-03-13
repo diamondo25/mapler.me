3febb9962b91fc40c5f9f280d1cb6124c1580fc1
diamondo25 <blablaechthema@hotmail.com>
2013-03-13 23:24:17 +0000

    - O.o  - _always_ escape database input with mysql_real_escape_string / $__database->real_escape_string  - Integers do not need to be escaped. Instead, use intval() to make it invalid (0) if not a valid value (like 'blah0'). '0003232nla' will be 3232 instead.
    git-svn-id: https://subversion.assembla.com/svn/mapler-me@326 7cbc8f23-9b70-4854-b091-be65f742de0c

--- trunk/website/inc/social.php
+++ trunk/website/inc/social.php
@@ -12,7 +12,7 @@ $antispam = true;
 if ($_loginaccount->GetConfigurationOption('last_status_sent', 0) != 0) {
 	$tmp = strtotime($_loginaccount->GetConfigurationOption('last_status_sent'));
 	$tmp += 1 * 60; // 1 minute
-	
+
 	if (time() < $tmp) { // 1 minute
 		$antispam = false;
 		$minutes_timeout = ceil(($tmp - time()) / 60);
@@ -21,44 +21,36 @@ if ($_loginaccount->GetConfigurationOption('last_status_sent', 0) != 0) {
 }
 
 // If antispam passes, push status
-if ($antispam && $_SERVER['REQUEST_METHOD'] == 'POST' && $_POST['content']) {
+if ($antispam && $_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['content'])) {
 	// Oh jolly
 	$antispam = false;
 	$minutes_timeout = 1;
 	$_loginaccount->SetConfigurationOption('last_status_sent', date("Y-m-d H:i:s"));
-	
-	$content = htmlentities($_POST['content']);
-	$contentfix = strip_tags($content);
-	$contentfix2 = stripslashes($contentfix);
-	$dc = htmlentities($_POST['dc']);
-	$dcfix = strip_tags($dc);
-	$dcfix2 = stripslashes($dcfix);
-	
+
+	$content = nl2br(htmlentities(strip_tags($_POST['content'])));
+	$dc = isset($_POST['dc']) ? 1 : 0;
+
 	$char_config = $_loginaccount->GetConfigurationOption('character_config', array('characters' => array(), 'main_character' => null));
 	$has_characters = !empty($char_config['main_character']);
-	
+
 	// set internally
 	$accid = $_loginaccount->GetId();
 	$nicknm = $_loginaccount->GetNickname();
-	
-	if ($has_characters):
-	$chr = $char_config['main_character'];
-	else:
-	$chr = '';
-	endif;
-	
-	$__database->query("INSERT INTO social_statuses VALUES (NULL, '".$accid."', '".$nicknm."', '".$chr."', '".$contentfix2."', '".$dcfix2."', NOW(), 0)");
+
+	$chr = $has_characters ? $char_config['main_character'] : '';
+
+
+	$__database->query("INSERT INTO social_statuses VALUES (NULL, ".$accid.", '".$__database->real_escape_string($nicknm)."', '".$__database->real_escape_string($chr)."', '".$__database->real_escape_string($content)."', ".$dc.", NOW(), 0)");
 ?>
 <p class="lead alert-success alert">Sending to Maple Admin.. checking.. success! Status posted.</p>
-<?php	
+<?php
 }
-
 elseif ($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['removeid'])) {
 	// Removing status
-	
-	$id = $__database->real_escape_string($_GET['removeid']);
-	
-	$__database->query("DELETE FROM social_statuses WHERE id = '".$id."' AND account_id = ".$_loginaccount->GetId());
+
+	$id = intval($_GET['removeid']);
+
+	$__database->query("DELETE FROM social_statuses WHERE id = ".$id." AND account_id = ".$_loginaccount->GetId());
 ?>
 <p class="lead alert-info alert">The status was successfully deleted.</p>
 <?php
@@ -71,16 +63,15 @@ elseif ($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['removeid'])) {
 <?php else: ?>
 
 <div class="row">
-	
-			<form method="post">
-					<div class="span9">
-					<textarea name="content" class="span9 status" style="height:100px;max-height:100px;max-width:888px;padding-right:50px;" placeholder="Type your status here!"></textarea>
-					</div>
-				<button type="submit" class="btn btn-large" style="padding:16px;position:relative;top:15px;">Post!</button>
-				<br/><br/><input type="checkbox" name="dc" value="1"/> Disable commenting?
-			</form>
-			
-		
+	<form method="post">
+		<div class="span9">
+			<textarea name="content" class="span9 status" style="height:100px; max-height:100px; max-width:888px; padding-right:50px;" placeholder="Type your status here!"></textarea>
+		</div>
+		<button type="submit" class="btn btn-large" style="padding:16px; position:relative; top:15px;">Post!</button>
+		<br />
+		<br />
+		<input type="checkbox" name="dc" value="1"/> Disable commenting?
+	</form>
 </div>
 
 <?php endif; ?>
