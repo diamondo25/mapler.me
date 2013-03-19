2477334fdac0f53bb8a8753cdd39b344b94daca5
thebluecorsair <tyler@tylerliberman.com>
2013-03-19 19:02:03 +0000

    - Hopefully this fixes it..
    git-svn-id: https://subversion.assembla.com/svn/mapler-me@369 7cbc8f23-9b70-4854-b091-be65f742de0c

--- trunk/website/stream/index.php
+++ trunk/website/stream/index.php
@@ -18,7 +18,12 @@ include('../inc/social.php');
 
 $q = $__database->query("
 SELECT
-	*,
+	id,
+	account_id,
+	nickname,
+	content,
+	character,
+	comments_disabled
 	TIMESTAMPDIFF(SECOND, timestamp, NOW()) AS `secs_since`
 FROM
 	social_statuses
@@ -52,12 +57,12 @@ foreach ($social_cache as $row) {
 			<div class="status<?php if ($row['override'] == 1): ?> notification<?php endif; ?>">
 				<div class="header">
 <?php if ($row['comments_disabled'] == '0'): ?>
-					<a href="//<?php echo $domain; ?>/stream/status/<?php echo $row['social_statuses.id']; ?>#disqus_thread"></a>
+					<a href="//<?php echo $domain; ?>/stream/status/<?php echo $row['id']; ?>#disqus_thread"></a>
 					<img src="//<?php echo $domain; ?>/inc/img/icons/comment.png"/> – 
 <?php endif; ?>
 					<a href="//<?php echo $row['username'];?>.<?php echo $domain; ?>/"><?php echo $row['nickname'];?></a> said:
 					<span class="pull-right">
-						<a href="//<?php echo $domain; ?>/stream/status/<?php echo $row['social_statuses.id']; ?>"><?php echo time_elapsed_string($row['secs_since']); ?> ago</a>
+						<a href="//<?php echo $domain; ?>/stream/status/<?php echo $row['id']; ?>"><?php echo time_elapsed_string($row['secs_since']); ?> ago</a>
 
 <?php
 	if ($_loggedin) {
