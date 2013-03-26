3d59de6620c64c868162d1d871169d6fca6764d7
thebluecorsair <tyler@tylerliberman.com>
2013-03-26 07:37:21 +0000

    - Fixes broken stream status.
    git-svn-id: https://subversion.assembla.com/svn/mapler-me@414 7cbc8f23-9b70-4854-b091-be65f742de0c

--- trunk/website/inc/stream.notice.template.php
+++ trunk/website/inc/stream.notice.template.php
@@ -3,7 +3,7 @@ $notice = file_get_contents('../actions/website/notice.txt');
 if ($notice !== '') {
 ?>
 <div class="status">
-<?php require_once __DIR__.'/../inc/stream.notice.template.php'; ?>
+<?php echo $notice; ?>
 </div>
 <?
 }
