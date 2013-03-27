ec8d8407708d9635a398a991926f89677923f41b
thebluecorsair <tyler@tylerliberman.com>
2013-03-27 09:16:40 +0000

    Just a minor change to ranks.
    Mapler.me+ or Mapler.me Plus membership? Just a suggestion as a name, fits well.
    
    git-svn-id: https://subversion.assembla.com/svn/mapler-me@426 7cbc8f23-9b70-4854-b091-be65f742de0c

--- trunk/website/inc/ranks.php
+++ trunk/website/inc/ranks.php
@@ -5,7 +5,10 @@ define("RANK_PERMABANNED", -200);
 define("RANK_BANNED", -100);
 define("RANK_AWAITING_ACTIVATION", 0);
 define("RANK_NORMAL", 100);
-define("RANK_DEVELOPER", 200);
+
+// Special
+define("RANK_PLUS", 200); //Membership
+define("RANK_DEVELOPER", 300);
 
 // Staff
 define("RANK_MODERATOR", 900);
