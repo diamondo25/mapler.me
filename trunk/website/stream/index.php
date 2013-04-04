554fa56550039e241fbf2a273c8d7e2e1878965d
thebluecorsair <tyler@tylerliberman.com>
2013-04-04 20:11:27 +0000

    LOL.
    git-svn-id: https://subversion.assembla.com/svn/mapler-me@464 7cbc8f23-9b70-4854-b091-be65f742de0c

--- trunk/website/stream/index.php
+++ trunk/website/stream/index.php
@@ -72,7 +72,7 @@ ORDER BY
 				<br />
 				<?php $parser->parse($content2); echo $parser->getAsHtml(); ?>
 				<div class="status-extra">
-					<?php if ($row['account_id'] !== 2): ?><a href="#" class="mention" onclick="Mention()" mentioned="<?php echo $row['username']; ?>"><i class="icon-share-alt"></i></a><?php endif; ?>
+					<?php if ($row['account_id'] !== 2): ?><a href="#" class="mention" onclick="Mention(<?php echo $row['username']; ?>)" mentioned="<?php echo $row['username']; ?>"><i class="icon-share-alt"></i></a><?php endif; ?>
 					<a href="//<?php echo $domain; ?>/stream/status/<?php echo $row['id']; ?>"><?php echo time_elapsed_string($row['secs_since']); ?> ago</a>
 
 <?php
