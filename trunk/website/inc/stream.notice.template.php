9f8d936d26b8a09e7eb11015f16be58451e88d0f
thebluecorsair <tyler@tylerliberman.com>
2013-04-04 19:04:41 +0000

    - Wat. Our "notice.txt" is being saved in /inc/ instead /manage/?
    git-svn-id: https://subversion.assembla.com/svn/mapler-me@458 7cbc8f23-9b70-4854-b091-be65f742de0c

--- trunk/website/inc/stream.notice.template.php
+++ trunk/website/inc/stream.notice.template.php
@@ -1,7 +1,11 @@
 <?php
-$notice = file_get_contents('../manage/notice.txt');
+$notice = file_get_contents('notice.txt');
+if ($notice !== '') {
 ?>
 	<div class="status">
 		<?php echo $notice; ?>
 	</div>
 <br/>
+<?php
+}
+?>
