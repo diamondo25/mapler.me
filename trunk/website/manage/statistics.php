2a92853a9d22e094593447e4b1f2325593372749
thebluecorsair <tyler@tylerliberman.com>
2013-04-09 06:55:02 +0000

    - Public Beta :: Statistics page
    git-svn-id: https://subversion.assembla.com/svn/mapler-me@496 7cbc8f23-9b70-4854-b091-be65f742de0c

---
+++ trunk/website/manage/statistics.php
@@ -0,0 +1,28 @@
+<?php
+require_once __DIR__.'/../inc/header.php';
+?>
+		<center><h1>Mapler.me Statistics</h1></center>
+			<h1><?php $q = $__database->query("SELECT COUNT(*) FROM accounts"); $tmp = $q->fetch_row(); $q->free(); echo $tmp[0]; ?> <span class="faded">accounts registered on Mapler.me.</span></h1>
+			
+			<h1><?php $q = $__database->query("SELECT COUNT(*) FROM characters"); $tmp = $q->fetch_row(); $q->free(); echo $tmp[0]; ?> <span class="faded">characters added by maplers.</span></h1>
+			
+			<h1><?php $q = $__database->query("SELECT COUNT(*) FROM items"); $tmp = $q->fetch_row(); $q->free(); echo $tmp[0]; ?> <span class="faded">items stored between all characters.</span></h1>
+			
+			<h1><?php $q = $__database->query("SELECT COUNT(*) FROM strings"); $tmp = $q->fetch_row(); $q->free(); echo $tmp[0]; ?> <span class="faded"> different items exist in MapleStory.</span></h1>
+			
+			<hr/>
+			
+			<h1><span class="faded">Mapler.me characters have been famed</span> <?php $q = $__database->query("SELECT COUNT(*) FROM timeline WHERE type = 'gotfame'"); $tmp = $q->fetch_row(); $q->free(); echo $tmp[0]; ?> <span class="faded">times together.</span></h1>
+			
+			<h1><span class="faded">Mapler.me characters have leveled</span> <?php $q = $__database->query("SELECT COUNT(*) FROM timeline WHERE type = 'levelup'"); $tmp = $q->fetch_row(); $q->free(); echo $tmp[0]; ?> <span class="faded">times collaboratively.</span></h1>
+			
+			<h1><span class="faded">Mapler.me characters have used</span> <?php $q = $__database->query("SELECT COUNT(*) FROM timeline WHERE type = 'jobup'"); $tmp = $q->fetch_row(); $q->free(); echo $tmp[0]; ?> <span class="faded">SP collaboratively.</span></h1>
+			
+			<hr/>
+			
+			<h1><?php $q = $__database->query("SELECT COUNT(*) FROM social_statuses"); $tmp = $q->fetch_row(); $q->free(); echo $tmp[0]; ?> <span class="faded">statuses posted between all members.</span></h1>
+			
+			<h1><?php $q = $__database->query("SELECT COUNT(*) FROM friend_list WHERE accepted_on IS NOT NULL"); $tmp = $q->fetch_row(); $q->free(); echo $tmp[0]; ?> <span class="faded">friendships have been formed.</span></h1>
+<?php
+require_once __DIR__.'/../inc/footer.php';
+?>
