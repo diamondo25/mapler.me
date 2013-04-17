706e882527fb19ce6ffd8ffad21ecb3722658534
diamondo25 <blablaechthema@hotmail.com>
2013-04-17 19:36:26 +0000

    - remove dem tabs
    git-svn-id: https://subversion.assembla.com/svn/mapler-me@570 7cbc8f23-9b70-4854-b091-be65f742de0c

--- trunk/website/manage/serverlog.php
+++ trunk/website/manage/serverlog.php
@@ -2,36 +2,37 @@
 require_once __DIR__.'/../inc/header.php';
 
 function RunCMD($cmd) {
-        $descriptorspec = array(
-           0 => array('pipe', 'r'),  // stdin is a pipe that the child will read from
-           1 => array('pipe', 'w'),  // stdout is a pipe that the child will write to
-           2 => array('pipe', 'w') 
-        );
+	echo 'COMMAND: '.$cmd."\r\n\r\n";
+	$descriptorspec = array(
+		0 => array('pipe', 'r'),  // stdin is a pipe that the child will read from
+		1 => array('pipe', 'w'),  // stdout is a pipe that the child will write to
+		2 => array('pipe', 'w') 
+	);
 
-        $cwd = '/mplrserver';
+	$cwd = '/mplrserver';
 
-        $process = proc_open($cmd, $descriptorspec, $pipes, $cwd);
+	$process = proc_open($cmd, $descriptorspec, $pipes, $cwd);
 
-        if (is_resource($process)) {
-                $data = stream_get_contents($pipes[1]);
-                fclose($pipes[1]);
+	if (is_resource($process)) {
+		$data = stream_get_contents($pipes[1]);
+		fclose($pipes[1]);
 
-                $return_value = proc_close($process);
-        }
-        else {
-                $data = "ERROR";
-        }
+		$return_value = proc_close($process);
+	}
+	else {
+		$data = 'ERROR';
+	}
 
-        return $data;
+	return $data;
 }
 
 $searchfor = '';
 $lines = 30;
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['searchfor'], $_POST['lines'])) {
-        $searchfor = $_POST['searchfor'];
-        $lines = intval($_POST['lines']);
-        if ($lines == 0) $lines = 30;
+	$searchfor = $_POST['searchfor'];
+	$lines = intval($_POST['lines']);
+	if ($lines == 0) $lines = 30;
 }
 
 ?>
@@ -45,11 +46,11 @@ Lines <input type="text" name="lines" value="<?php echo $lines; ?>" /><br />
 $oldestTime = 0;
 $name = '';
 foreach (glob('/mplrserver/logs/*') as $filename) {
-        $t = filectime($filename);
-        if ($t > $oldestTime) {
-                $oldestTime = $t;
-                $name = $filename;
-        }
+	$t = filectime($filename);
+	if ($t > $oldestTime) {
+		$oldestTime = $t;
+		$name = $filename;
+	}
 }
 ?>
 <pre>
