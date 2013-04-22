299fd3b0a4e0242c98baf4735d6aa2ee46b4b2e0
thebluecorsair <tyler@tylerliberman.com>
2013-04-22 23:48:24 +0000

    - Updated and improved blog. - Has a "player spotlight" on the sidebar, as well as a link to Leafre.net (temporary affiliate).
    git-svn-id: https://subversion.assembla.com/svn/mapler-me@601 7cbc8f23-9b70-4854-b091-be65f742de0c

--- trunk/website/blog/index.php
+++ trunk/website/blog/index.php
@@ -10,9 +10,22 @@ require_once __DIR__.'/../inc/header.php';
 <div class="row">
 <div class="span8" id="statuslist"></div>
 
-<div class="span3">
--place sidebar here-
-</div>
+<div class="span4">
+	<div class="status">
+		<center>
+			<h2>Player Spotlight</h2>
+			<img src="//mapler.me/ignavatar/Marissaurus"/><br/>
+			(<a href="//maryse.mapler.me">Maryse</a>)<hr />
+			Maryse, one of our Mapler+ members, joined Mapler.me early on during it's private beta. Since the very beginning she helped give suggestions for additions, helped discover and tackle some early bugs with our client, as well as being an active member.
+		</center>
+	</div>
+	
+		<div class="status">
+		<center>
+			<h2><img src="//cdn.mapler.me/etc/leafre.png"/></h2>
+			Leafre.net is a community dedicated on KMS (Korean MapleStory) owned by <a href="http://kremechoco.mapler.me/" target="_blank">KremeChoco</a> that offers maplers the ability to play with other korean maplers. <a href="http://leafre.net/" target="_blank">Check them out!</a>
+		</center>
+	</div>
 </div>
 
 <script>
