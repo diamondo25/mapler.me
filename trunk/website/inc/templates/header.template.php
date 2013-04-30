abfe7a6efe311c38b001539d6497194d7556eabb
thebluecorsair <tyler@tylerliberman.com>
2013-04-30 06:44:45 +0000

    - Now can be added as a web app on iOS (not sure if it changes anything for Android)
    git-svn-id: https://subversion.assembla.com/svn/mapler-me@659 7cbc8f23-9b70-4854-b091-be65f742de0c

--- trunk/website/inc/templates/header.template.php
+++ trunk/website/inc/templates/header.template.php
@@ -23,6 +23,8 @@ if ($_loggedin) {
 	<title><?php echo $title; ?></title>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<meta name="apple-mobile-web-app-capable" content="yes" />
+	<meta name="apple-mobile-web-app-status-bar-style" content="black" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
 	<meta name="keywords" content="maplestory, maple, story, mmorpg, maple story, maplerme, mapler, me, Mapler Me, Mapler.me, Nexon, Nexon America,
 	henesys, leafre, southperry, maplestory rankings, maplestory, realtime updates, Maplestory items, MapleStory skills, guild, alliance, GMS, KMS, EMS, <?php
