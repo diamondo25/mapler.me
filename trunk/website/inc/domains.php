230b188cdf9270e731c3e5ae4daacc4db000721d
diamondo25 <blablaechthema@hotmail.com>
2013-05-11 18:49:51 +0000

    - fixfixfix
    git-svn-id: https://subversion.assembla.com/svn/mapler-me@726 7cbc8f23-9b70-4854-b091-be65f742de0c

--- trunk/website/inc/domains.php
+++ trunk/website/inc/domains.php
@@ -1,34 +1,36 @@
 <?php
-$subdomain = "";
-$domain = "";
+$subdomain = '';
+$domain = '';
 
-if (strpos($_SERVER['SERVER_NAME'], "direct.mapler.me") !== false) {
+if (strpos($_SERVER['SERVER_NAME'], 'direct.mapler.me') !== false) {
 	// SOMETHING.direct.mapler.me
-	$subdomain = substr($_SERVER['SERVER_NAME'], 0, strrpos($_SERVER['SERVER_NAME'], ".direct.mapler.me"));
-	$domain = "direct.mapler.me";
+	$subdomain = substr($_SERVER['SERVER_NAME'], 0, strrpos($_SERVER['SERVER_NAME'], '.direct.mapler.me'));
+	$domain = 'direct.mapler.me';
 }
-elseif (strpos($_SERVER['SERVER_NAME'], "mapler.me") !== false) {
+elseif (strpos($_SERVER['SERVER_NAME'], 'mapler.me') !== false) {
 	// SOMETHING.mapler.me
-	$subdomain = substr($_SERVER['SERVER_NAME'], 0, strrpos($_SERVER['SERVER_NAME'], ".mapler.me"));
-	$domain = "mapler.me";
+	$subdomain = substr($_SERVER['SERVER_NAME'], 0, strrpos($_SERVER['SERVER_NAME'], '.mapler.me'));
+	$domain = 'mapler.me';
 }
-elseif (strpos($_SERVER['SERVER_NAME'], "mplrtest.craftnet.nl") !== false) {
+elseif (strpos($_SERVER['SERVER_NAME'], 'mplrtest.craftnet.nl') !== false) {
 	// SOMETHING.mplrtest.craftnet.nl << Test Case Server
-	$subdomain = substr($_SERVER['SERVER_NAME'], 0, strrpos($_SERVER['SERVER_NAME'], ".mplrtest.craftnet.nl"));
-	$domain = "mplrtest.craftnet.nl";
+	$subdomain = substr($_SERVER['SERVER_NAME'], 0, strrpos($_SERVER['SERVER_NAME'], '.mplrtest.craftnet.nl'));
+	$domain = 'mplrtest.craftnet.nl';
 }
-elseif (strpos($_SERVER['SERVER_NAME'], "mplr.e.craftnet.nl") !== false) {
+elseif (strpos($_SERVER['SERVER_NAME'], 'mplr.e.craftnet.nl') !== false) {
 	// SOMETHING.mplr.e.craftnet.nl << Test Case Erwin
-	$subdomain = substr($_SERVER['SERVER_NAME'], 0, strrpos($_SERVER['SERVER_NAME'], ".mplr.e.craftnet.nl"));
-	$domain = "mplr.e.craftnet.nl";
+	$subdomain = substr($_SERVER['SERVER_NAME'], 0, strrpos($_SERVER['SERVER_NAME'], '.mplr.e.craftnet.nl'));
+	$domain = 'mplr.e.craftnet.nl';
 }
-
-elseif (strpos($_SERVER['SERVER_NAME'], "maplerme") !== false) {
+elseif (strpos($_SERVER['SERVER_NAME'], 'maplerme') !== false) {
 	// SOMETHING.maplerme << Local Testing Tyler
-	$subdomain = substr($_SERVER['SERVER_NAME'], 0, strrpos($_SERVER['SERVER_NAME'], ".maplerme"));
-	$domain = "maplerme";
+	$subdomain = substr($_SERVER['SERVER_NAME'], 0, strrpos($_SERVER['SERVER_NAME'], '.maplerme'));
+	$domain = 'maplerme';
 }
 
 $subdomain = strtolower(trim($subdomain));
+if (isset($_SERVER['HTTP_ORIGIN']) && strpos($_SERVER['HTTP_ORIGIN'], $domain) !== FALSE) {
+	header('Access-Control-Allow-Origin: '.$_SERVER['HTTP_ORIGIN']);
+}
 
 ?>
