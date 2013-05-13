2691165997ff8d2fa926cead07b3dc85aaff5e80
diamondo25 <blablaechthema@hotmail.com>
2013-05-13 22:30:37 +0000

    - Just , '30' :)
    git-svn-id: https://subversion.assembla.com/svn/mapler-me@739 7cbc8f23-9b70-4854-b091-be65f742de0c

--- trunk/website/manage/statuses.php
+++ trunk/website/manage/statuses.php
@@ -2,11 +2,11 @@
 require_once __DIR__.'/../inc/header.php';
 
 $statusses = new Statusses();
-$statusses->Load('blog = 0');
+$statusses->Load('blog = 0', '30');
 
 foreach ($statusses->data as $status) {
-		$status->PrintAsHTML('');
-	}
+	$status->PrintAsHTML('');
+}
 
 require_once __DIR__.'/../inc/footer.php';
 ?>
