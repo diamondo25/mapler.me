9419f0dab54cc504d0da7dcfe214e1bc0fcf265f
thebluecorsair <tyler@tylerliberman.com>
2013-05-20 15:02:10 +0000

    - Doesn't expect an exact match.
    git-svn-id: https://subversion.assembla.com/svn/mapler-me@764 7cbc8f23-9b70-4854-b091-be65f742de0c

--- trunk/website/manage/findstring.php
+++ trunk/website/manage/findstring.php
@@ -3,7 +3,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['thing'], $_GET['ajax']
 	header('Content-type: application/json');
 	require_once __DIR__.'/../inc/classes/database.php';
 	$searching = $__database->real_escape_string($_POST['thing']);
-	$q = $__database->query("SELECT objecttype, objectid, `key`, `value` FROM strings WHERE `value` LIKE '".$searching."' LIMIT 100");
+	$q = $__database->query("SELECT objecttype, objectid, `key`, `value` FROM strings WHERE `value` LIKE '%".$searching."%' LIMIT 100");
 	
 	$tmp = array();
 	while ($row = $q->fetch_assoc())
