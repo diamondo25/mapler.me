f5d77c4fbd277d8d8b55642fbe50cb6e47e87a96
diamondo25 <blablaechthema@hotmail.com>
2013-07-11 23:13:49 +0000

    AND suddenly ,
    git-svn-id: https://subversion.assembla.com/svn/mapler-me@914 7cbc8f23-9b70-4854-b091-be65f742de0c

--- trunk/website/passreset.php
+++ trunk/website/passreset.php
@@ -29,11 +29,8 @@ if (isset($_GET['code'])) {
 			
 			$encryptedpassword = GetPasswordHash($_POST['password'], $salt);
 			
-			echo $encryptedpassword."<br />";
-			echo "UPDATE accounts SET password = '".$encryptedpassword."' AND salt = '".$__database->real_escape_string($salt)."' WHERE id = ".$id;
-			
 			// $__database->query("DELETE FROM account_tokens WHERE code = '".$code."' AND type = 'password_reset'");
-			$__database->query("UPDATE accounts SET password = '".$encryptedpassword."' AND salt = '".$__database->real_escape_string($salt)."' WHERE id = ".$id);
+			$__database->query("UPDATE accounts SET `password` = '".$encryptedpassword."', salt = '".$__database->real_escape_string($salt)."' WHERE id = ".$id);
 		
 ?>
 <p class="lead alert-success alert">Password reset!</p>
