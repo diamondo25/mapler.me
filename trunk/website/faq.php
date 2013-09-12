4e526ab176ab3710221e7110427f688aca4d193f
thebluecorsair <tyler@tylerliberman.com>
2013-09-12 08:13:39 +0000

    - FAQ overhaul.
    git-svn-id: https://subversion.assembla.com/svn/mapler-me@1005 7cbc8f23-9b70-4854-b091-be65f742de0c

--- trunk/website/faq.php
+++ trunk/website/faq.php
@@ -19,11 +19,72 @@ border-radius: 0 3px 3px 0;
 .faq li {
 margin-bottom: 20px;
 }
+
+.ct-label a {
+    color: #FFF !important;
+}
 </style>
+
+<?php if(isset($_GET['id'])) {
+?>
 <div class="row">
+<?php
+    $faqid = intval($_GET['id']);
+    
+    $q = $__database->query("
+    SELECT
+    	*
+    FROM
+    	faq
+    WHERE
+        id = $faqid
+    ");
+    
+    $questions = array();
+    while ($row = $q->fetch_assoc()) {
+    	$questions[] = $row;
+    }
+
+if ($q->num_rows == 0) {
+	$q->free();
+?>
+<div class="span12">
+<center>
+	<p class="lead status">Question not found.</p>
+</center>
+</div>
+<?php
+	require_once __DIR__.'/inc/footer.php';
+	die;
+}
+    
+    $q->free();
+    foreach ($questions as $row) {
+?>
+<div class="faq span12">
+        <div class="pull-right">
+			<a href="//<?php echo $domain; ?>/faq/" class="btn btn-default"><i class="icon-chevron-left"></i> Return to the FAQ</a>
+		</div>
+		<h3 style="margin-bottom:20px;" class="alert alert-info"><i class="icon-question-sign"></i> Question: <?php echo $row['title']; ?></h3>
+		<p class="lead"><?php echo $row['content']; ?></p>
+</div>
+<?php
+}
+?>
+</div>
+<?php
+	require_once __DIR__.'/inc/footer.php';
+    die;
+}
+?>
+
+<div class="row">
+
 <div class="faq span12" style="margin-bottom:30px;">
 		<div class="center">
 			<img src="http://cdn.mapler.me/media/67549be4c8a03cbd248d1c97410a18e8.png" style="border-radius:5px;"/>
+			<p>Have a question? Start typing a topic or word you're thinking about, and it may appear below!<br />
+			If not, feel free to <a href="//<?php echo $domain; ?>/support/">let us know</a> and we'll add it here!</p>
 			<input type="text" value="" placeholder="Start typing ...">
 		</div>
 </div>
@@ -46,7 +107,7 @@ $q->free();
 foreach ($questions as $row) {
 ?>
 			<li style="display:none;">
-				<h3><?php echo $row['title']; ?> <span class="ct-label"><?php echo $row['topic']; ?></span></h3>
+				<h3><?php echo $row['title']; ?> <span class="ct-label"><a href="//<?php echo $domain; ?>/faq/<?php echo $row['id']; ?>"><i class="icon-share-sign"></i> Direct Link</a></span></h3>
 				<p><?php echo $row['content']; ?></p>
 			</li>
 <?php
