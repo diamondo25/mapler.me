89face08197e2465d7c7ae3f3e67747ba1c23f01
thebluecorsair <tyler@tylerliberman.com>
2013-10-05 11:08:30 +0000

    - New footer, cleaned up as well. - Removed google ads (will find another place, likely Stream sidebar).
    git-svn-id: https://subversion.assembla.com/svn/mapler-me@1057 7cbc8f23-9b70-4854-b091-be65f742de0c

--- trunk/website/inc/footer.php
+++ trunk/website/inc/footer.php
@@ -1,31 +1,14 @@
 		</div>
 		<div style="clear:both;"></div>
 		<footer>
-			<p><span><i class="icon-asterisk"></i> 2012-<?php echo date('Y'); ?> Mapler.me</span></p>
-			<p><a href="//status.mapler.me">Status</a> - <a href="//<?php echo $domain; ?>/faq/">FAQ</a> - <a href="//<?php echo $domain; ?>/contributions/">Contributions</a> - <a href="//<?php echo $domain; ?>/support/">Support</a></p>
-			<br />
-			
-			<?php
-				//Displays queries ran / APC caching status for admins only.
-				if ($_loggedin && $_loginaccount->GetAccountRank() == RANK_ADMIN) {
-			?>
-			
-			<p class="faded"><?php echo $__database->QueriesRan(); ?> queries ran - <?php echo function_exists('apc_fetch') ? 'Using APC caching' : 'NOT using APC caching'; ?></p>
-			
-			<?php
-			}
-			?>
-			
-<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- Sidebar -->
-<ins class="adsbygoogle"
-     style="display:inline-block;width:234px;height:60px"
-     data-ad-client="ca-pub-6550692695111418"
-     data-ad-slot="4246038926"></ins>
-<script>
-(adsbygoogle = window.adsbygoogle || []).push({});
-</script>
-
+			<ul>
+				<li><a href="//status.mapler.me">Status</a></li>
+				<li><a href="//<?php echo $domain; ?>/faq/">FAQ</a></li>
+				<li><a href="//<?php echo $domain; ?>/contributions/">Contributions</a></li>
+				<li><a href="//<?php echo $domain; ?>/support/">Support</a></li>
+				<br />
+				<li class="copyright">&copy; <?php echo date('Y'); ?> Mapler.me / MapleStory &copy; Nexon America, Europe</li>
+			</ul>
 		</footer>
 	<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/modernizr/2.6.2/modernizr.min.js"></script>	
 
